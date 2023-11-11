401779ff382556228bd71a9cea6ee69909298a31
Алексей Компанец <trueflywood@gmail.com>
2023-11-11 10:36:22 +0300

    menu

--- app/src/main/java/com/example/flywood/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/flywood/kotlin/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.flywood.kotlin
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 
@@ -10,6 +13,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val constraintLayout = findViewById(R.id.constraintLayout) as ConstraintLayout
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.my_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        var id = item.itemId
+        when (id) {
+            R.id.edit -> Toast.makeText(this, "edit ", Toast.LENGTH_SHORT).show()
+            R.id.file -> {
+                var check = item.isChecked
+                item.setChecked(!check)
+                Toast.makeText(this, "file ", Toast.LENGTH_SHORT).show()
+            }
+            R.id.view -> Toast.makeText(this, "view ", Toast.LENGTH_SHORT).show()
+            R.id.help -> Toast.makeText(this, "help ", Toast.LENGTH_SHORT).show()
+
+            else -> { // Note the block
+                print("x is neither 1 nor 2")
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
